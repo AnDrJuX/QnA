05fa4d4261041289766c47f8412e06d97f9e63fb
Andres Peterson <andres_p@mail.ru>
2017-09-20 21:10:40 +0300

    add question id to db/migrate/20170920175624_add_question_id_to_answers.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170920161452) do
+ActiveRecord::Schema.define(version: 20170920175624) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,8 @@ ActiveRecord::Schema.define(version: 20170920161452) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "question_id"
+    t.index ["question_id"], name: "index_answers_on_question_id"
   end
 
   create_table "questions", force: :cascade do |t|
