7bf49aca931cfa58abb1a7b7beec3f247432331e
Andres Peterson <andres_p@mail.ru>
2017-09-26 18:29:44 +0300

    add sign_in, out, up_spec.rb green test

--- spec/acceptance/sign_in_spec.rb
+++ spec/acceptance/sign_in_spec.rb
@@ -11,7 +11,7 @@ feature 'Siging in', %q{
   scenario 'Existing user try to sign in' do
     #sign_in(user)
       visit root_path
-      click_on 'LogIN'
+      click_on 'Log in'
       fill_in 'Email', with: user.email
       fill_in 'Password', with: user.password
       click_on 'Log in'
