0cb09d52e5a4c5662e3b7ec7512afbe91879f0a7
Andres Peterson <andres_p@mail.ru>
2017-09-26 18:20:41 +0300

    add to spec/acceptance/sign_in_spec.rb green spec

--- spec/acceptance/sign_in_spec.rb
+++ spec/acceptance/sign_in_spec.rb
@@ -6,22 +6,27 @@ feature 'Siging in', %q{
   I want be able to sign in
  } do
 
-  given(:user) { create(:user) }
+  given(:user) {create(:user)}
 
   scenario 'Existing user try to sign in' do
-    sign_in(user)
+    #sign_in(user)
+      visit root_path
+      click_on 'LogIN'
+      fill_in 'Email', with: user.email
+      fill_in 'Password', with: user.password
+      click_on 'Log in'
 
-    expect(page).to have_content 'Signed in successfully.'
-    expect(current_path).to eq root_path
+      expect(page).to have_content 'Signed in successfully.'
+      expect(current_path).to eq root_path
+    end
+
+    scenario 'Non-existing user try to sign in' do
+      visit new_user_session_path
+      fill_in 'Email', with: 'wrong@user.com'
+      fill_in 'Password', with: '12345'
+      click_on 'Log in'
+
+      expect(page).to have_content 'Invalid Email or password.'
+      expect(current_path).to eq new_user_session_path
+    end
   end
-
-  scenario 'Non-existing user try to sign in' do
-    visit new_user_session_path
-    fill_in 'Email', with: 'wrong@user.com'
-    fill_in 'Password', with: '12345'
-    click_on 'Log in'
-
-    expect(page).to have_content 'Invalid Email or password. Log in Email Password Remember me Sign up Forgot your password?'
-    expect(current_path).to eq new_user_session_path
-  end
-end
