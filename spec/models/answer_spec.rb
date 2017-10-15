84e0d8524d575827f93def2999a8bf033e59ca9d
Andres Peterson <andres_p@mail.ru>
2017-10-15 10:11:23 +0300

    add chenge to spec/models/answer_spec.rb & answers_controller_spec.rb

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it {should validate_presence_of :body}
+  it {should belong_to(:question)}
+  it {should validate_length_of(:body).is_at_least(5).on(:create)}
+
+  describe '#check_best' do
+    let(:user) {create(:user)}
+    let(:question) {create(:question, user: user)}
+    let(:answer) {create(:answer, question: question, user: user)}
+
+    it 'change attribute best to true' do
+      answer.check_best
+      expect(answer).to be_best
+    end
+  end
 end
